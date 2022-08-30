86373bbeaeba670479b43f5c658d6577a1aa7bb2
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-08-30 10:45:34 +0300

    create models

---
+++ app/Http/Controllers/ParticipantController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreParticipantRequest;
+use App\Http\Requests\UpdateParticipantRequest;
+use App\Models\Participant;
+
+class ParticipantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreParticipantRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreParticipantRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateParticipantRequest  $request
+     * @param  \App\Models\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateParticipantRequest $request, Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Participant $participant)
+    {
+        //
+    }
+}
